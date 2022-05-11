d0bba1f40c98a8cb3d73d9158e2a30bf75ffed69
Anderson cardenas <andyguaca@gmail.com>
2022-05-11 12:18:32 -0500

    adapter and view holders implements

--- app/src/main/java/com/example/catchpokemon/utils/PokemonAdapter.kt
+++ app/src/main/java/com/example/catchpokemon/utils/PokemonAdapter.kt
@@ -1,4 +1,48 @@
 package com.example.catchpokemon.utils
 
-class PokemonAdapter {
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.catchpokemon.R
+import com.example.catchpokemon.model.Pokemon
+
+class PokemonAdapter: RecyclerView.Adapter<PokemonView>(), PokemonView.OnShowPokemon {
+
+    private val pokedex = ArrayList<Pokemon>()
+    private val imgs = ArrayList<Bitmap>()
+    lateinit var listener : PokemonView.OnShowPokemon
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonView {
+        val inflater = LayoutInflater.from(parent.context)
+        val item = inflater.inflate(R.layout.view_pokemon, parent, false)
+        return PokemonView(item)
+    }
+
+    override fun onBindViewHolder(holder: PokemonView, position: Int) {
+        val pokemon = pokedex[position]
+        holder.listener = this
+        holder.pokemon = pokemon
+        holder.imageRow.setImageBitmap(imgs[position])
+        holder.nameRow.text = pokemon.name
+        holder.dateRow.text = pokemon.date.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return pokedex.size
+    }
+
+    override fun show(pokemon: Pokemon) {
+        listener.show(pokemon)
+    }
+    fun addPokemon(pokemon:Pokemon, img:Bitmap){
+        pokedex.add(pokemon)
+        imgs.add(img)
+    }
+
+    fun deletePokemon(pokemon: Pokemon) {
+        val index = pokedex.indexOf(pokemon)
+        pokedex.removeAt(index)
+        imgs.removeAt(index)
+    }
 }
--- app/src/main/java/com/example/catchpokemon/utils/PokemonView.kt
+++ app/src/main/java/com/example/catchpokemon/utils/PokemonView.kt
@@ -1,4 +1,27 @@
 package com.example.catchpokemon.utils
 
-class PokemonView {
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.catchpokemon.R
+import com.example.catchpokemon.model.Pokemon
+
+class PokemonView(itemView: View): RecyclerView.ViewHolder(itemView) {
+    lateinit var pokemon:Pokemon
+    var listener : OnShowPokemon? = null
+        set(value) {
+            field = value
+            imageRow.setOnClickListener{
+                listener?.show(pokemon)
+            }
+        }
+    var nameRow : TextView = itemView.findViewById(R.id.nameRow)
+    var dateRow : TextView = itemView.findViewById(R.id.dateRow)
+    var imageRow : ImageButton = itemView.findViewById(R.id.imageRow)
+
+    interface OnShowPokemon{
+        fun show(pokemon:Pokemon)
+    }
+
 }
